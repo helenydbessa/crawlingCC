1df98ec9e81d13c2b29798c61ff89d38968bc29e
Heleny <heleny.bessa@gmail.com>
2021-12-27 16:29:50 -0300

    salva na pasta

--- webscraper.py
+++ webscraper.py
@@ -1,23 +1,37 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError, URLError
 from bs4 import BeautifulSoup
+import os
 from webcrawler import linkList
 
 #print(linkList)
-charExcludedSpace = ["BTS (방탄소년단) – ", ":", "$"]
+charExcludedSpace = ["BTS (방탄소년단) – ", ":", "$", "?", "\"", "/", "="]
 for link in linkList:
     try:
         html = urlopen(link)
     except HTTPError as e:
-        print(e)
+        print("ERRO ", e)
     bs = BeautifulSoup(html.read(), 'html.parser')
     name = bs.h1.text
     for c in charExcludedSpace:
         name = name.replace(c, "")
+    #presentPath = os.path.dirname(__file__)
+    folder = 'lyrics'
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    folderPath = 'lyrics/'
+    fileName = f'{name}.txt'
+    wholePath = os.path.join(folderPath, fileName)
+    print(wholePath)
+    text = open(wholePath, "w", encoding="utf-8")
     if bs.find('div', {'class':'wp-block-column is-vertically-aligned-top'}) == None:
-        print(bs.find('div', {'class':'card-body'}).find_all('td'))
-        text = open(f"{name}.txt", "w")
-        text.write(bs.find('div', {'class':'card-body'}).find_all('td'))
+        result = bs.find('div', {'class':'card-body'}).find_all('div', {'class':'entry-content'})
+        # print(result)
+        for r in result:
+            for t in r.find_all('td'):
+                text.write(str(t.text))
+        #text.write(bs.find('div', {'class':'card-body'}).find_all('td'))
         text.close()
     else:
-        print(bs.find('div', {'cl""ass':'wp-block-column is-vertically-aligned-top'}))
+        print("else")
+        print(bs.find('div', {'class':'wp-block-column is-vertically-aligned-top'}))
